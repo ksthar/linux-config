65ef6f8bd9ef1e533612b430341b900086249e46
Harlan Shoop <harlan@kstechnologies.com>
2014-07-22 07:05:07 -0600

    Added cygwin support to tmux right.sh script

--- .tmux/right.sh
+++ .tmux/right.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 MYHOST=$(hostname -s)
-MYDATE=$(date | cut -c 1-3)' '$(date | cut -c 12-16)
 MYEXTIP=$(curl http://whatismyip.akamai.com/)
 #BATTLEFT=$(acpi -b | awk '{print$5}')
 #BATTSTATE=$(acpi -b | awk '{print$3}' )
 #if [ $BATTSTATE = "Unknown," ]; then 
 #	BATTLEFT="Plugged-in" 
 #fi
-MYINTIP=$(ifconfig eth0 | awk '/inet\ / { print $2 }' | awk '{ split( $0,a,":"); print a[2]}' | tail -1)
+if [ "$(uname -o)" == "Cygwin" ]; then
+	MYINTIP=$(ipconfig | awk '/IPv4\ / { print $14 }' | head -1)
+	MYDATE=$(date | cut -c 1-3)' '$(date | cut -c 19-23);
+else
+	MYINTIP=$(ifconfig eth0 | awk '/inet\ / { print $2 }' | awk '{ split( $0,a,":"); print a[2]}' | tail -1)
+	MYDATE=$(date | cut -c 1-3)' '$(date | cut -c 12-16);
+fi	
 #echo '#[fg=white]Battery: #[fg=yellow]#[bg=black] '$BATTLEFT\
 echo ' #[fg=white]#[bg=colour237] Hostname: #[fg=yellow]#[bg=black] '$MYHOST\
 ' #[fg=white]#[bg=colour237] Int: #[fg=yellow]#[bg=black] '$MYINTIP\
