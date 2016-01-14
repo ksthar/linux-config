e57aef5e1b41f05f907e807e2d1e8f11a587b947
Harlan Shoop <harlan@kstechnologies.com>
2016-01-14 15:02:27 +0000

    made right.sh more universal

--- .tmux/right.sh
+++ .tmux/right.sh
@@ -1,33 +1,79 @@
 #!/bin/bash
+#
+# 2016-01-14
+# ----------------------------------------------------------------------
+# Print the right status line for tmux.  This script collects the 
+# hostname, local IP, external IP, and date/time for display.  Tmux
+# runs this script every few seconds.
+#
+# Scripting is used to make this file as universal as possible.  
+# Every system has its idiosyncracies, and this file attempts to 
+# adapt itself to the environment it is installed in.
+# 
+# Format:
+#   battery | hostname | int IP | ext IP | date/time
+#
+# ----------------------------------------------------------------------
 
+
+# ----------------------------------------------------------------------
+# Hostname
+# ----------------------------------------------------------------------
+# This may be changed below
 MYHOST=$(hostname -s)
-MYEXTIP=$(curl http://whatismyip.akamai.com/)
-# Print time remaining
-#BATTLEFT=$(acpi -b | awk '{print$5}')
-# Print percent remaining
-#BATTLEFT=$(acpi -b | awk '{print$4}' | cut -f1 -s -d,)
-# Or both...
-#BATTLEFT=$(acpi -b | awk '{print$4}' | cut -f1 -s -d,)' - '$(acpi -b | awk '{print$5}')
-#BATTSTATE=$(acpi -b | awk '{print$3}' )
-#if [ $BATTSTATE = "Unknown," ]; then 
-#	BATTLEFT="Plugged-in";
-#fi
-#MYINTIP=$(ifconfig wlan0 | awk '/inet\ / { print $2 }' | awk '{ split( $0,a,":"); print a[2]}' | tail -1)
-#echo '#[fg=white]Battery: #[fg=yellow]#[bg=black] '$BATTLEFT\
-#	' #[fg=white]#[bg=colour237] Hostname: #[fg=yellow]#[bg=black] '$MYHOST\
 
-#BATTSTATE=$(acpi -b | awk '{print$3}' )
-#if [ $BATTSTATE = "Unknown," ]; then 
-#	BATTLEFT="Plugged-in" 
-#fi
+# ----------------------------------------------------------------------
+# Find External IP
+# ----------------------------------------------------------------------
+# if this isn't working, make sure curl is installed and you are 
+# connected to the internet...
+MYEXTIP=$(curl http://whatismyip.akamai.com/)
+
+# ----------------------------------------------------------------------
+# Find Battery
+# ----------------------------------------------------------------------
+# only do this if we have acpi installed
+if [ `acpi -b | awk '/command not found/ {x=1} END {print 1-x}'` == 0 ]; then
+    # Print time remaining
+    #BATTLEFT=$(acpi -b | awk '{print$5}')
+
+    # Print percent remaining
+    #BATTLEFT=$(acpi -b | awk '{print$4}' | cut -f1 -s -d,)
+
+    # Or both...
+    BATTLEFT=$(acpi -b | awk '{print$4}' | cut -f1 -s -d,)' - '$(acpi -b | awk '{print$5}')
+    BATTSTATE=$(acpi -b | awk '{print$3}' )
+    if [ $BATTSTATE = "Unknown," ]; then 
+    	BATTLEFT="Plugged-in";
+    fi
+
+    echo '#[fg=white]Battery: #[fg=yellow]#[bg=black] '$BATTLEFT
+fi
+
+# ----------------------------------------------------------------------
+# Find local IP and date/time
+# ----------------------------------------------------------------------
+# If we're using cygwin, adjust ipconfig processing
 if [ "$(uname -o)" == "Cygwin" ]; then
 	MYINTIP=$(ipconfig | awk '/IPv4\ / { print $14 }' | head -1)
 	MYDATE=$(date +"%D")' '$(date | cut -c 1-3)' '$(date | cut -c 19-23);
 else
-	MYINTIP=$(ifconfig eth0 | awk '/inet\ / { print $2 }' | awk '{ split( $0,a,":"); print a[2]}' | tail -1)
+    # if we're using an edison, check IP of wlan0 and fix hostname
+    if [ `uname -a | awk '/edison/ {x=1} END {print 1-x}'` == 0 ]; then
+        MYHOST=$(hostname)
+        MYINTIP=$(ifconfig wlan0 | awk '/inet\ / { print $2 }' | awk '{ split( $0,a,":"); print a[2]}' | tail -1)
+
+    # for stock linux, just check eth0
+    else
+        MYINTIP=$(ifconfig eth0 | awk '/inet\ / { print $2 }' | awk '{ split( $0,a,":"); print a[2]}' | tail -1)
+
+    fi
 	MYDATE=$(date +"%D")' '$(date | cut -c 1-3)' '$(date | cut -c 12-16);
 fi	
-#echo '#[fg=white]Battery: #[fg=yellow]#[bg=black] '$BATTLEFT\
+
+# ----------------------------------------------------------------------
+# Print line
+# ----------------------------------------------------------------------
 echo ' #[fg=white]#[bg=colour237] Hostname: #[fg=yellow]#[bg=black] '$MYHOST\
 ' #[fg=white]#[bg=colour237] Int: #[fg=yellow]#[bg=black] '$MYINTIP\
 ' #[fg=white]#[bg=colour237] Ext: #[fg=yellow]#[bg=black] '$MYEXTIP\
