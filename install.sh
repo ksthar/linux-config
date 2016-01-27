9e3043738c7b092da431d41eb00c065b97b81dd5
Harlan Shoop <harlan@kstechnologies.com>
2016-01-26 21:49:48 -0700

    Added more installs; this installs git and linux-config--that may not ever be necessary

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# -------------------------------------------------------------------
+# @file     install.sh
+# @brief    This script is designed to get an Ubuntu system up and 
+#           running quickly, automating routing install tasks.
+#           
+# @author   Harlan Shoop
+# @version  1.0
+# @date     2016-01-26
+# -------------------------------------------------------------------
+#
 # Escaped Color Codes:
 # --------------------
 # 030m Black
@@ -10,13 +20,40 @@
 # 036m Cyan
 # 037m White
 
+# Start up message
 printf "\033[034m";
-echo "------------------------------"
-echo "     Har's System Setup"
-echo "------------------------------"
+echo "-------------------------------------------------------------------"
+echo "                      Har's System Setup"
+echo "-------------------------------------------------------------------"
+printf "\033[036m";
+echo "This script is tailored to an Ubuntu system.  If are using something"
+echo "else, you may need to make adjustments."
+echo ""
+echo "              Press [ENTER] to continue, Ctrl-C to quit..."
+read
+echo ""
+
+# Install apps here; make sure dependencies are handled now
 printf "\033[037m";
-echo "> Installing Standard Apps:"
-echo "------------------------------"
+echo "> (1 of 5) Installing Standard Apps:"
+echo "-------------------------------------------------------------------"
+printf "\033[032m";
+echo "... (prep) update"
+printf "\033[036m";
+sudo apt-get update
+printf "\033[032m";
+echo "... (prep) upgrade"
+printf "\033[036m";
+sudo apt-get upgrade
+printf "\033[032m";
+echo "... (prep) autoremove"
+printf "\033[036m";
+sudo apt-get autoremove
+printf "\033[032m";
+echo "... git"
+printf "\033[036m";
+sudo apt-get install git
+printf "\033[032m";
 printf "\033[032m";
 echo "... vim-nox"
 printf "\033[036m";
@@ -28,10 +65,44 @@ sudo apt-get install tmux
 printf "\033[032m";
 echo ""
 
-
+# Setup git config
 printf "\033[037m";
-echo "> Setting up symlinks:"
-echo "------------------------------"
+echo "> (2 of 5) Setting up git:"
+echo "-------------------------------------------------------------------"
+printf "\033[036m";
+cd linux-config
+git config --global user.name   "Harlan Shoop"
+git config --global user.email  "harlan@kstechnologies.com"
+git config --global color.ui    "auto"
+git config --global core.editor "vim"
+git config credential.helper    store
+
+# list updated config
+git config --global -l
+echo ""
+
+# Install any required repos; here we're installing the linux-config repo
+# this is circular since we can only get this from installing linux-config,
+# but it covers the case where all we have is this script.  We may adjust
+# this later.
+printf "\033[037m";
+echo "> (3 of 5) Installing repos:"
+echo "-------------------------------------------------------------------"
+printf "\033[032m";
+echo "... linux-config"
+printf "\033[036m";
+cd $HOME
+git clone https://github.com/ksthar/linux-config.git
+# If we already have it installed, let's update to the latest
+cd $HOME/linux-config
+git pull origin master
+printf "\033[032m";
+echo ""
+
+# Setup all the symlinks to the included configs
+printf "\033[037m";
+echo "> (4 of 5) Setting up symlinks:"
+echo "-------------------------------------------------------------------"
 printf "\033[032m";
 cd $HOME
 echo "... bashrc"
@@ -65,31 +136,16 @@ ln -s linux-config/.tmux.conf
 echo ""
 
 printf "\033[037m";
-echo "> Sourcing new bashrc..."
-echo "------------------------------"
+echo "> (5 of 5) Sourcing new bashrc..."
+echo "-------------------------------------------------------------------"
 printf "\033[032m";
 source $HOME/.bashrc
 echo "... Done"
 echo ""
 
-printf "\033[037m";
-echo "> Setting up git:"
-echo "------------------------------"
-printf "\033[036m";
-cd linux-config
-git config --global user.name   "Harlan Shoop"
-git config --global user.email  "harlan@kstechnologies.com"
-git config --global color.ui    "auto"
-git config --global core.editor "vim"
-git config credential.helper    store
-
-# list updated config
-git config --global -l
-echo ""
-
 printf "\033[034m";
-echo "------------------------------"
-echo "            EOF"
-echo "------------------------------"
+echo "-------------------------------------------------------------------"
+echo "                              EOF"
+echo "-------------------------------------------------------------------"
 printf "\033[037m";
 echo ""
