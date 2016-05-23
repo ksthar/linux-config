07ad188b7012e89e7960a2222a0f0c56e599aac0
Ubuntu <ubuntu@ip-172-31-17-208.us-west-2.compute.internal>
2016-05-23 20:55:41 +0000

    Changing the name to reflect letsencrypt => certbot

---
+++ certbot_update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# -------------------------------------------------------------
+# @file     certbot_update.sh
+# @brief    Script to renew LetsEncrypt certificates.  This is
+#           designed to be run from cron periodically.  A log
+#           message is posted to the KST log server with the
+#           result.  Local log file renewal.log is also 
+#           updated.
+#
+# @author   Harlan Shoop
+# @version  1.1
+# @date     2016-05-23
+# -------------------------------------------------------------
+
+# setup log variables
+MYNAME="Test Site"
+MESSAGE="Certificate renewal script ran with no errors"
+TYPE="log"
+
+# Run the renewal script
+sudo /home/ubuntu/certbot/certbot-auto renew -nvv > /var/log/letsencrypt/renewal.log 2>&1
+
+# Parse the result
+LE_STATUS=$?
+if [ "$LE_STATUS" != 0 ]; then
+    TYPE="error"
+    MESSAGE="Certificate Renewal Failed. See renewal.log for details"
+fi
+
+# log the result; NOTE: log server sec group must accept connections from this host
+curl -H "Content-Type: application/json" -X POST -d '{"type":"'"$TYPE"'","module":"certbot","application":"'"$MYNAME"'","message":"'"$MESSAGE"'"}' "http://logs.kstechnologies.com:8106/log"
